d4f8f5ff9d75a55e11056d106ae970725a392130
Kyle Hazen <kyle@Kyles-iMac.local>
2020-07-06 22:05:31 -0700

    add ingredients with call back onClick -> props.added -> props.ingredientAdded -> addIngredientHandler in BurgerBuilder where the burger state is changed.

--- burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,24 +1,47 @@
 import React, { useState } from "react";
 import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacone: 0.7
+}
 
 const BurgerBuilder = () => {
 
     const [burgerState, setBurgerState] = useState(
         {
             ingredients: {
-                salad: 1,
-                bacon: 1,
-                cheese: 2,
-                meat: 2
-            }
+                salad: 0,
+                bacon: 0,
+                meat: 0,
+                cheese: 0
+            },
+            totalPrice: 4
         }
     )
+
+    const addIngredientHandler = (type) => {
+        const oldCount = burgerState.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...burgerState.ingredients
+        };
+        updatedIngredients[type] = updatedCount
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = burgerState.totalPrice;
+        const newPrice = oldPrice + priceAddition
+        setBurgerState({ingredients: updatedIngredients, totalPrice: newPrice})
+    }
 
     return (
         <Aux>
             <Burger ingredients={burgerState.ingredients}/>
-            <div>Build Controls</div>
+            <BuildControls
+                ingredientAdded={addIngredientHandler}/>
         </Aux>
     );
 };
